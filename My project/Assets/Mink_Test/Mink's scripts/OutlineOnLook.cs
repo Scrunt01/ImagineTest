7fd4319dfcc797d26e942a2d2114928291c2f942
Johan <johanvdtak@proton.me>
2026-03-25 22:47:13 +0100

    De outline werkt!

--- My project/Assets/Mink_Test/Mink's scripts/OutlineOnLook.cs
+++ My project/Assets/Mink_Test/Mink's scripts/OutlineOnLook.cs
@@ -2,17 +2,78 @@ using UnityEngine;
 
 public class OutlineOnLook : MonoBehaviour
 {
-    public Material mat;
-    public int indexOfMat = 1;
+    // This script is on the player
 
-    private void Awake()
-    {
-        mat = GetComponent<Renderer>().materials[indexOfMat];
-        Debug.Log("Material Index ding");
+    
+    [SerializeField] int indexOfMat = 1;
+
+    [SerializeField] Camera cam;
+
+    private bool inBenchArea = false;
+    private Material shaderMat;
+
+    public bool IsOutlineActive { get { return isOutlineActive; } 
+        set
+        {
+            if (value == isOutlineActive) return;
+
+            isOutlineActive = value;
+            SetOutline(value);
+        }
+
     }
 
-    public void Outline(bool show)
+    private bool isOutlineActive;
+
+    public void SetOutline(bool show)
     {
-        mat.SetFloat("_ShowOutline", show ? 1 : 0);
+        Debug.Log(System.Convert.ToSingle(show));
+        shaderMat.SetFloat("_ShowOutLine", System.Convert.ToSingle(show));
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (inBenchArea)
+        {
+            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+            Debug.DrawRay(cam.transform.position, cam.transform.forward);
+
+            if (Physics.Raycast(ray, out var hit))
+            {
+                if (hit.transform.gameObject.CompareTag("Bench"))
+                {
+                    IsOutlineActive = true;
+                }
+                else
+                {
+                    IsOutlineActive = false;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bench"))
+        {
+            shaderMat = other.gameObject.GetComponentInParent<Renderer>().materials[indexOfMat];
+            inBenchArea = true;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bench"))
+        {
+
+            inBenchArea = false;
+            IsOutlineActive = false;
+        }
     }
 }
